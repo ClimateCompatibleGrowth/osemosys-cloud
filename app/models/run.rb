bb29b28d6dc2826602f9a366b9e8477b6d816c21
Youssef Boulkaid <youssef.boulkaid@bukowskis.com>
2018-12-26 00:58:56 +0100

    Add method to solve run locally

--- app/models/run.rb
+++ app/models/run.rb
@@ -1,4 +1,8 @@
 class Run < ApplicationRecord
   has_one_attached :model_file
   has_one_attached :data_file
+
+  def solve_locally!
+    Osemosys::SolveModel.new(s3_data_key: data_file.key, s3_model_key: model_file.key).call
+  end
 end
