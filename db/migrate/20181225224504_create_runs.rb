c71b0650d1c8334d6e61a9f6fc849b5dbbba04c8
Youssef Boulkaid <youssef.boulkaid@bukowskis.com>
2018-12-25 23:46:24 +0100

    Create runs

---
+++ db/migrate/20181225224504_create_runs.rb
@@ -0,0 +1,10 @@
+class CreateRuns < ActiveRecord::Migration[5.2]
+  def change
+    create_table :runs do |t|
+      t.string :name
+      t.datetime :started_at
+      t.datetime :finished_at
+      t.timestamps
+    end
+  end
+end
