22ad2dae34eeaef782d1c69bb42c50719cf4914a
Youssef Boulkaid <youssef.boulkaid@bukowskis.com>
2018-12-26 03:17:17 +0100

    Remove unnecessary requires

--- lib/osemosys.rb
+++ lib/osemosys.rb
@@ -3,16 +3,6 @@ require 'aws-sdk-s3'
 require 'dry-configurable'
 require 'tty-command'
 
-# Not necessary anymore since rails autoloads them
-# but leaving them in case we want to extract a gem
-require_relative 'osemosys/config'
-require_relative 'osemosys/workfile'
-require_relative 'osemosys/data_file'
-require_relative 'osemosys/ec2_instance'
-require_relative 'osemosys/model_file'
-require_relative 'osemosys/output_file'
-require_relative 'osemosys/solve_model'
-
 # TODO: Save and upload logs
 
 module Osemosys
