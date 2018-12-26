f9d8d2a5f2ded27639b46bc6fe973630cf659c22
Youssef Boulkaid <youssef.boulkaid@bukowskis.com>
2018-12-26 03:39:45 +0100

    Format index as a table

--- app/models/run.rb
+++ app/models/run.rb
@@ -3,9 +3,8 @@ class Run < ApplicationRecord
   has_one_attached :data_file
   has_one_attached :result_file
 
-  def solve_locally!
-    # Move to service object
-    solved_file = Osemosys::SolveModel.new(s3_data_key: data_file.key, s3_model_key: model_file.key).call
-    result_file.attach(io: solved_file, filename: File.basename(solved_file.to_path))
+  def solving_time
+    return unless started_at && finished_at
+    finished_at - started_at
   end
 end
