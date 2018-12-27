c7b480dae4f709c4c8957f6dd3e0ccbc15831855
Youssef Boulkaid <youssef.boulkaid@bukowskis.com>
2018-12-28 00:57:28 +0100

    Centralize duration and date formatting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def format_duration(duration)
+    Time.at(duration).utc.strftime('%H:%M:%S')
+  end
+
+  def format_datetime(datetime)
+    datetime.strftime('%e %b %Y %H:%M:%S')
+  end
 end
