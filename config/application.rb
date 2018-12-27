361bcb343532d9967a90b5c05121c3fa72f4c855
Youssef Boulkaid <youssef.boulkaid@bukowskis.com>
2018-12-28 00:57:04 +0100

    Use the right time zone

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module OsemosysCloud
   class Application < Rails::Application
+    config.time_zone = 'Europe/Stockholm'
+
     config.load_defaults 5.2
   end
 end
