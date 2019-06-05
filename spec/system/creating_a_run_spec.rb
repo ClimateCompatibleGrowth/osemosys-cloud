d52ac9fa1950b77e4d41124e3f50d5b850ba35c3
Youssef Boulkaid <yboulkaid@gmail.com>
2019-06-05 21:21:39 +0200

    First system spec

---
+++ spec/system/creating_a_run_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe 'Creating a run', type: :system do
+  before do
+    driven_by(:rack_test)
+  end
+
+  it 'works' do
+    visit root_path
+
+    expect(page).to have_text('Osemosys Cloud')
+
+    click_on 'New run'
+
+    expect(page).to have_text('Schedule a new run')
+
+    fill_in 'Name', with: 'Atlantis'
+
+    atlantis_model = "#{Rails.root}/spec/data/atlantis_model.txt"
+    atlantis_data = "#{Rails.root}/spec/data/atlantis_data.txt"
+
+    attach_file 'run_model_file', atlantis_model
+    attach_file 'run_data_file', atlantis_data
+
+    click_on 'Create run'
+
+    expect(page).to have_text 'Atlantis'
+  end
+end
