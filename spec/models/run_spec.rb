b7ffb13d049b017c16af2deebf1c5ad4bb806697
Youssef Boulkaid <yboulkaid@gmail.com>
2019-06-05 21:02:30 +0200

    Add first specs

---
+++ spec/models/run_spec.rb
@@ -0,0 +1,27 @@
+RSpec.describe Run do
+  describe '#solving_time' do
+    context 'when the run has not been started' do
+      it 'is nil' do
+        run = Run.new(started_at: nil)
+
+        expect(run.solving_time).to be nil
+      end
+    end
+
+    context 'when the run has not been finished' do
+      it 'is nil' do
+        run = Run.new(started_at: 1.day.ago, finished_at: nil)
+
+        expect(run.solving_time).to be nil
+      end
+    end
+
+    context 'when the run has started and finished' do
+      it 'is the time difference between start and finish' do
+        run = Run.new(started_at: 1.hour.ago, finished_at: Time.current)
+
+        expect(run.solving_time).to be_within(1).of(3600)
+      end
+    end
+  end
+end
