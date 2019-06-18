22308eae97e2ec54f588b2a1a9cdd442a30427ff
Youssef Boulkaid <yboulkaid@gmail.com>
2019-06-18 22:51:41 +0200

    Add description to runs

---
+++ db/migrate/20190618205048_add_description_to_runs.rb
@@ -0,0 +1,5 @@
+class AddDescriptionToRuns < ActiveRecord::Migration[5.2]
+  def change
+    add_column :runs, :description, :text, null: false, default: ''
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_27_185939) do
+ActiveRecord::Schema.define(version: 2019_06_18_205048) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2018_12_27_185939) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "queued_at"
+    t.text "description", default: "", null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
