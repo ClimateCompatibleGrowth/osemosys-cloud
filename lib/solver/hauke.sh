015ea9b14be256f812518c8300af845a46fabd19
Youssef Boulkaid <yboulkaid@gmail.com>
2019-06-19 18:00:11 +0200

    Update Hauke instructions

--- lib/solver/hauke.sh
+++ lib/solver/hauke.sh
@@ -1,6 +1,13 @@
 # From AMI
+mkdir workspace
+sudo mkfs -t xfs /dev/nvme1n1
+sudo mount /dev/nvme1n1 workspace -o umask=000
+sudo chown -R ec2-user workspace/
+cd workspace
+tmux 
+
 aws s3 cp "s3://osemosys-playground/hauke/Commit2_edHH.txt" Commit2_edHH.txt
-aws s3 cp "s3://osemosys-playground/hauke/OSeMBE_V2_C0T0E0_data_190617.txt" OSeMBE_V2_C0T0E0_data_190617.txt
+aws s3 cp "s3://osemosys-playground/hauke/OSeMBE_V2_C1T0E1_data.txt" OSeMBE_V2_C1T0E1_data.txt
 glpsol -m Commit2_edHH.txt -d OSeMBE_V2_C0T0E0_data_190617.txt --wlp input.lp --check
 
 
@@ -11,4 +18,4 @@ unzip CloudWatchMonitoringScripts-1.2.2.zip && \
 rm CloudWatchMonitoringScripts-1.2.2.zip && \
 cd aws-scripts-mon
 crontab -e 
-# */1 * * * * ~/aws-scripts-mon/mon-put-instance-data.pl --mem-util --mem-used --mem-avail --from-cron
+# */1 * * * * ~/workspace/aws-scripts-mon/mon-put-instance-data.pl --mem-util --mem-used --mem-avail --from-cron
