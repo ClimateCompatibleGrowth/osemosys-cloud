6225c4318613da554d49694fac1f3c681dffc715
Youssef Boulkaid <yboulkaid@gmail.com>
2019-06-30 12:59:59 +0200

    Add sendgrid credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,16 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { host: 'osemosys-cloud.herokuapp.com' }
 
+  ActionMailer::Base.smtp_settings = {
+    user_name: Rails.application.credentials.dig(:sendgrid, :username),
+    password: Rails.application.credentials.dig(:sendgrid, :password),
+    domain: 'http://osemosys-cloud.herokuapp.com',
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
