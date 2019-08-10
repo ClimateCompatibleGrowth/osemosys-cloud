7dce77cf203d0737c03a666a45a8f4408fd33eab
Youssef Boulkaid <yboulkaid@gmail.com>
2019-08-10 23:52:52 +0200

    Remove possibility to solve using glpk

--- spec/models/osemosys/solve_cbc_model_spec.rb
+++ spec/models/osemosys/solve_cbc_model_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe Osemosys::SolveModel do
+RSpec.describe Osemosys::SolveCbcModel do
   context 'with a model and data' do
     it 'solves the model with glpsol' do
       atlantis_model_path = "#{Rails.root}/spec/data/atlantis_model.txt"
       atlantis_data_path = "#{Rails.root}/spec/data/atlantis_data.txt"
 
-      output_file = Osemosys::SolveModel.new(
+      output_file = Osemosys::SolveCbcModel.new(
         local_model_path: atlantis_model_path,
         local_data_path: atlantis_data_path
       ).call
 
-      expect(output_file).to eq('./output.txt.gz')
+      expect(output_file).to eq('./data/output.sol.gz')
     end
   end
 end
