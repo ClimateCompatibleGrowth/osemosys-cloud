63c568ab01ce3f6831400f37abc3ee48d38ac65f
Youssef Boulkaid <yboulkaid@gmail.com>
2019-08-10 23:48:36 +0200

    Add spec for faulty run

--- spec/system/running_a_run_spec.rb
+++ spec/system/running_a_run_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 require 'rake'
 
 RSpec.describe 'Running a run' do
-  it 'Solves the run' do
+  it 'Solves a valid run' do
     User.create!(email: 'test@example.com', password: 'blehbleh')
     run = create_run
 
@@ -21,6 +21,27 @@ RSpec.describe 'Running a run' do
     expect(run.outcome).to eq('success')
   end
 
+  it 'Handles a faulty run' do
+    User.create!(email: 'test@example.com', password: 'blehbleh')
+    run = create_faulty_run
+
+    expect(run.queued_at).to be nil
+    expect(run.started_at).to be nil
+    expect(run.finished_at).to be nil
+    expect(run.outcome).to be nil
+
+    OsemosysCloud::Application.load_tasks
+    expect {
+      Rake::Task['solve_cbc_run'].invoke(run.id)
+    }.to raise_error(TTY::Command::ExitError)
+
+    run.reload
+    expect(run.queued_at).to be nil
+    expect(run.started_at).to be_past
+    expect(run.finished_at).to be_past
+    expect(run.outcome).to eq('failure')
+  end
+
   def create_run
     run = Run.create!(
       name: 'My run',
@@ -36,6 +57,26 @@ RSpec.describe 'Running a run' do
     run.model_file.attach(
       io: File.open("#{Rails.root}/spec/data/atlantis_model.txt"),
       filename: 'atlantis_model.txt'
+    )
+
+    run
+  end
+
+  def create_faulty_run
+    run = Run.create!(
+      name: 'My run',
+      description: 'My description',
+      user_id: User.last.id
+    )
+
+    run.data_file.attach(
+      io: File.open("#{Rails.root}/spec/data/empty_file.txt"),
+      filename: 'empty_file.txt'
+    )
+
+    run.model_file.attach(
+      io: File.open("#{Rails.root}/spec/data/empty_file.txt"),
+      filename: 'empty_file.txt'
     )
 
     run
