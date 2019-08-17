b71fe11dd080ae555d6414640ed2760ba0a63ed3
Youssef Boulkaid <yboulkaid@gmail.com>
2019-08-17 21:18:55 +0200

    Add sidekiq web

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   devise_for :users
+
+  authenticate :user, ->(u) { u.id == 1 } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
+
   root to: 'pages#home'
   resources :runs, only: %i[new create index]
   post '/run/:id/start', to: 'runs#start', as: :start_run
