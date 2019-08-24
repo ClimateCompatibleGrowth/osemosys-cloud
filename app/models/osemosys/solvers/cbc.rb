d8f53d227e76ae42dfb8a9dd26d84c1d5115a86e
Youssef Boulkaid <yboulkaid@gmail.com>
2019-08-24 12:24:13 +0200

    Create solvers module

---
+++ app/models/osemosys/solvers/cbc.rb
@@ -0,0 +1,67 @@
+module Osemosys
+  module Solvers
+    class Cbc
+      def initialize(local_model_path:, local_data_path:, logger: Config.logger)
+        @local_data_path = local_data_path
+        @local_model_path = local_model_path
+        @logger = logger
+      end
+
+      def call
+        generate_input_file
+        find_solution
+        zip_output
+        print_summary
+
+        zipped_output_path
+      end
+
+      private
+
+      attr_reader :local_data_path, :local_model_path, :logger
+
+      def generate_input_file
+        Commands::GenerateInputFile.new(
+          local_model_path: local_model_path,
+          local_data_path: local_data_path,
+          lp_path: lp_path,
+          logger: logger,
+        ).call
+      end
+
+      def find_solution
+        Commands::FindSolution.new(
+          lp_path: lp_path,
+          output_path: output_path,
+          logger: logger,
+        ).call
+      end
+
+      def zip_output
+        Commands::Zip.new(
+          source: output_path,
+          destination: zipped_output_path,
+          logger: logger,
+        ).call
+      end
+
+      def print_summary
+        logger.info 'Model solved!'
+        logger.info ''
+        logger.info "run_id: #{Config.run_id}"
+      end
+
+      def lp_path
+        "./data/input_#{Config.run_id}.lp"
+      end
+
+      def output_path
+        "./data/output_#{Config.run_id}.txt"
+      end
+
+      def zipped_output_path
+        "./data/output_#{Config.run_id}.zip"
+      end
+    end
+  end
+end
--- spec/models/osemosys/solvers/cbc_spec.rb
+++ spec/models/osemosys/solvers/cbc_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe Osemosys::SolveCbcModel do
+RSpec.describe Osemosys::Solvers::Cbc do
   context 'with a model and data' do
     it 'solves the model with glpsol' do
       atlantis_model_path = "#{Rails.root}/spec/data/atlantis_model.txt"
       atlantis_data_path = "#{Rails.root}/spec/data/atlantis_data.txt"
 
-      output_file = Osemosys::SolveCbcModel.new(
+      output_file = Osemosys::Solvers::Cbc.new(
         local_model_path: atlantis_model_path,
         local_data_path: atlantis_data_path,
       ).call
