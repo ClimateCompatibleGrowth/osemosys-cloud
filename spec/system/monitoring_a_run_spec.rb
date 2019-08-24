132fea3532ec140c6d478e6070261a2c89a6cd9d
Youssef Boulkaid <yboulkaid@gmail.com>
2019-08-24 10:44:38 +0200

    Update monitoring run spec

--- spec/system/monitoring_a_run_spec.rb
+++ spec/system/monitoring_a_run_spec.rb
@@ -12,23 +12,19 @@ RSpec.describe 'Creating a run', type: :system do
 
     visit runs_path
 
-    expect(page).to have_content('Run number 1 (Not started yet)')
-    within('.timeline__item--current') do
-      expect(page).to have_content('Created')
-    end
+    expect(page).to have_content('Run number 1 (New)')
+    expect(page).to have_content('Created')
 
     expect(page).to have_content('Start run')
 
-    run.update(queued_at: Time.current)
     run.transition_to(:queued)
     visit current_path
 
-    expect(page).to have_content('Run number 1 (In queue)')
+    expect(page).to have_content('Run number 1 (Queued)')
     within('.timeline__item--current') do
       expect(page).to have_content('Queued')
     end
 
-    run.update(started_at: Time.current)
     run.transition_to(:ongoing)
     visit current_path
 
@@ -37,7 +33,6 @@ RSpec.describe 'Creating a run', type: :system do
       expect(page).to have_content('Ongoing')
     end
 
-    run.update(finished_at: Time.current)
     run.update(outcome: 'success')
     run.transition_to(:succeeded)
     visit current_path
