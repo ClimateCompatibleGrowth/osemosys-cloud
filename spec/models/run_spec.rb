3599fba6f9466bdb06f1ebd65e61af73c2733c28
Youssef Boulkaid <yboulkaid@gmail.com>
2019-08-24 10:42:10 +0200

    Update run_spec

--- spec/models/run_spec.rb
+++ spec/models/run_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Run do
   describe '#solving_time' do
     context 'when the run has not been started' do
       it 'is nil' do
-        run = Run.new(started_at: nil)
+        run = build(:run)
 
         expect(run.solving_time).to be nil
       end
@@ -12,7 +12,7 @@ RSpec.describe Run do
 
     context 'when the run has not been finished' do
       it 'is nil' do
-        run = Run.new(started_at: 1.day.ago, finished_at: nil)
+        run = build(:run, :ongoing)
 
         expect(run.solving_time).to be nil
       end
@@ -20,7 +20,8 @@ RSpec.describe Run do
 
     context 'when the run has started and finished' do
       it 'is the time difference between start and finish' do
-        run = Run.new(started_at: 1.hour.ago, finished_at: Time.current)
+        run = create(:run, :finished)
+        run.history.first.update(created_at: 1.hour.ago)
 
         expect(run.solving_time).to be_within(1).of(3600)
       end
