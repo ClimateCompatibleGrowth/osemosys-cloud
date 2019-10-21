0783a04fe116c2410af8c21813a318038f7a6a90
Youssef Boulkaid <yboulkaid@gmail.com>
2019-10-21 15:33:10 +0200

    Add a flash animation when refreshing

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery/dist/jquery.min.js
 //= require popper.js/dist/umd/popper.min.js
 //= require bootstrap/dist/js/bootstrap.js
+//= require vendor/jquery.color-2.1.2.min.js
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
