4210942a8f1581ff5255da7bb88132f024a52ce7
Youssef Boulkaid <yboulkaid@gmail.com>
2019-10-27 15:28:50 +0100

    Add mailer info

--- app/mailers/run_mailer.rb
+++ app/mailers/run_mailer.rb
@@ -1,4 +1,6 @@
 class RunMailer < ApplicationMailer
+  add_template_helper(ApplicationHelper)
+
   def run_finished_email
     @run = params[:run]
     mail(to: @run.user.email, subject: 'Run finished')
