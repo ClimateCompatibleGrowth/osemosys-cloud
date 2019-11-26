9ff38b34c182dedf8153b38c6c0a6b7980a12cca
Youssef Boulkaid <yboulkaid@gmail.com>
2019-11-26 08:46:14 +0100

    Paginate and order user's runs in admin view

--- app/controllers/admin/runs_controller.rb
+++ app/controllers/admin/runs_controller.rb
@@ -3,6 +3,12 @@ module Admin
     def index
       @user = User.find(params[:user_id])
       @runs = @user.runs
+        .order(id: :desc)
+        .page(params[:page])
+        .with_attached_model_file
+        .with_attached_data_file
+        .with_attached_result_file
+        .with_attached_log_file
     end
   end
 end
