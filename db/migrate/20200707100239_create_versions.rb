2e9836291ba843a30bf85a8f7db7b9fbee81979e
Youssef Boulkaid <yboulkaid@gmail.com>
2020-07-07 13:09:33 +0200

    Migrate old data

--- db/migrate/20200707100239_create_versions.rb
+++ db/migrate/20200707100239_create_versions.rb
@@ -7,5 +7,12 @@ class CreateVersions < ActiveRecord::Migration[6.0]
     end
 
     add_reference :runs, :version, foreign_key: true
+
+    Version.transaction do
+      Run.all.group_by(&:user_id).each do |user_id, runs|
+        version = Version.create!(name: 'Old runs', user_id: user_id)
+        runs.each { |run| run.update(version: version) }
+      end
+    end
   end
 end
