df43c07b1118161b0f4afcc95e31ecad50222414
Youssef Boulkaid <yboulkaid@gmail.com>
2020-08-02 12:14:12 +0200

    Add ServerType

---
+++ app/models/server_type.rb
@@ -0,0 +1,25 @@
+class ServerType
+  def self.all
+    [
+      new(name: 'Small server', slug: 'sidekiq'),
+      new(name: 'Large server', slug: 'z1d.3xlarge'),
+    ].freeze
+  end
+
+  def self.to_enum_definition
+    all.map do |server_type|
+      { server_type.slug => server_type.slug }
+    end
+  end
+
+  def self.from_slug(slug)
+    all.find { |server_type| server_type.slug == slug }
+  end
+
+  def initialize(name:, slug:)
+    @name = name
+    @slug = slug
+  end
+
+  attr_reader :name, :slug
+end
