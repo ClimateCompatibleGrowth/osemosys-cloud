361527d5d833c5969575daa662588f55c901218d
Youssef Boulkaid <yboulkaid@gmail.com>
2020-09-07 22:05:04 +0200

    Use older syntax

--- app/assets/javascripts/copy_to_clipboard.js
+++ app/assets/javascripts/copy_to_clipboard.js
@@ -1,7 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
   clipboardIcons = document.querySelectorAll('[data-copy-to-clipboard]');
   clipboardIcons.forEach(function(clipboardIcon) {
-    clipboardIcon.addEventListener('click', event => {
+    clipboardIcon.addEventListener('click', (event) => {
       navigator.clipboard.writeText(clipboardIcon.dataset.copyToClipboard)
       event.preventDefault()
     })
