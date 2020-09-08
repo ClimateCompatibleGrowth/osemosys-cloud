c18c806418baf309fd1f93c3d3a512e53cc987f7
Youssef Boulkaid <yboulkaid@gmail.com>
2020-09-08 17:57:16 +0200

    Make clipboard a button and log to console

--- app/assets/javascripts/copy_to_clipboard.js
+++ app/assets/javascripts/copy_to_clipboard.js
@@ -2,6 +2,7 @@ window.addEventListener('DOMContentLoaded', () => {
   clipboardIcons = document.querySelectorAll('[data-copy-to-clipboard]');
   clipboardIcons.forEach(function(clipboardIcon) {
     clipboardIcon.addEventListener('click', event => {
+      console.log(clipboardIcon.dataset.copyToClipboard);
       navigator.clipboard.writeText(clipboardIcon.dataset.copyToClipboard)
       event.preventDefault()
     })
