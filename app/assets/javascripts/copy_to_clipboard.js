eb0da8b3d5b65fc6c7d4b8798b2d413ffaf93e78
Youssef Boulkaid <yboulkaid@gmail.com>
2020-09-08 18:29:05 +0200

    Toggle tooltip

--- app/assets/javascripts/copy_to_clipboard.js
+++ app/assets/javascripts/copy_to_clipboard.js
@@ -2,8 +2,9 @@ window.addEventListener('DOMContentLoaded', () => {
   clipboardIcons = document.querySelectorAll('[data-copy-to-clipboard]');
   clipboardIcons.forEach(function(clipboardIcon) {
     clipboardIcon.addEventListener('click', event => {
-      console.log(clipboardIcon.dataset.copyToClipboard);
       navigator.clipboard.writeText(clipboardIcon.dataset.copyToClipboard)
+      $(clipboardIcon).tooltip('show')
+      setTimeout(() => { $(clipboardIcon).tooltip('hide') }, 1000)
       event.preventDefault()
     })
   })
