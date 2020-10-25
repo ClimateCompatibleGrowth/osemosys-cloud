873d8acd2098d23d8b4bdebc8d6b038b2c7e47d3
Youssef Boulkaid <yboulkaid@gmail.com>
2020-10-25 11:58:52 +0100

    Fix spec

--- spec/models/language_spec.rb
+++ spec/models/language_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe Language do
   describe '.to_enum_definition' do
     it 'is the list of all types as an enum definition' do
       expect(Language.to_enum_definition).to eq(
-        'English' => 'en',
-        'Spanish' => 'es',
+        'en' => 'en',
+        'es' => 'es',
       )
     end
   end
