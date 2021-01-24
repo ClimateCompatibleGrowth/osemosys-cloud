ac21c4d53bf0208076a7eb3665bf5d14a16f2f77
Youssef Boulkaid <yboulkaid@gmail.com>
2021-01-24 20:00:35 +0100

    Remove translation
    This reverts commit f42b322dc666567a4a14c1deb83afef8fef88627.
    
    # Conflicts:
    #	Gemfile.lock
    #	config/credentials.yml.enc

--- config/initializers/translation.rb
+++
@@ -1,6 +0,0 @@
-TranslationIO.configure do |config|
-  config.api_key        = Rails.application.credentials.dig(:translation, :api_key)
-  config.source_locale  = 'en'
-  config.target_locales = ['es']
-  config.disable_gettext = true
-end
