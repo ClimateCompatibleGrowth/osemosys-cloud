29f73eaadc11016aec1680a054b43b5fcd5217b7
Youssef Boulkaid <yboulkaid@gmail.com>
2021-01-30 16:19:15 +0100

    Add job to generate res

---
+++ app/jobs/generate_res_job.rb
@@ -0,0 +1,37 @@
+class GenerateResJob < ActiveJob::Base
+  queue_as :osemosys
+  sidekiq_options retry: 0
+
+  def perform(run_id)
+    @run = Run.find(run_id)
+
+    Commands::GenerateRes.new(
+      local_data_path: model_and_data.local_data_path,
+      res_path: res_path,
+      logger: logger,
+    ).call
+    run.res_file.attach(
+      io: File.open("#{res_path}.pdf"),
+      filename: File.basename(res_path),
+    )
+  end
+
+  private
+
+  attr_reader :run
+
+  def model_and_data
+    @model_and_data ||= Osemosys::DownloadModelFromS3.new(
+      run: run,
+      logger: logger,
+    ).call
+  end
+
+  def res_path
+    "res-#{run.id}"
+  end
+
+  def logger
+    Logger.new($stdout)
+  end
+end
---
+++ app/models/commands/generate_res.rb
@@ -0,0 +1,30 @@
+module Commands
+  class GenerateRes
+    def initialize(local_data_path:, res_path:, logger:)
+      @local_data_path = local_data_path
+      @res_path = res_path
+      @logger = logger
+    end
+
+    def call
+      logger.info 'Generating RES file'
+      tty_command.run(generate_res_command)
+    end
+
+    def generate_res_command
+      %(
+      python3 #{Rails.root.join('scripts/osemosys_res/osemosys_RES.py')}
+              #{local_data_path}
+              #{res_path}
+      ).delete("\n")
+    end
+
+    private
+
+    attr_reader :local_data_path, :res_path, :logger
+
+    def tty_command
+      @tty_command ||= TTY::Command.new(output: logger, color: false)
+    end
+  end
+end
