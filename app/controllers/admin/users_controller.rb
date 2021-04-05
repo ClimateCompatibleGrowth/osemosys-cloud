f1f0cdaa79bb8de6d9ef98ef11f8b895ae1b6ad5
Youssef Boulkaid <yboulkaid@gmail.com>
2021-04-05 12:58:09 +0200

    Show tags in index

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,7 +4,7 @@ module Admin
       respond_to do |format|
         @q = User.ransack(params[:q])
         @q.sorts = 'id asc' if @q.sorts.empty?
-        @users = @q.result.includes(:runs).page(params[:page]).per(50)
+        @users = @q.result.includes(:runs, tags: :taggings).page(params[:page]).per(50)
 
         format.html
         format.csv do
