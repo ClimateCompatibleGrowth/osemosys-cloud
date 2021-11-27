3f58495048a53aa7084c0c02bb51259a2d6fa129
Youssef Boulkaid <yboulkaid@gmail.com>
2021-11-27 17:15:20 +0100

    Add locale to visualization_url

--- app/models/run_result.rb
+++ app/models/run_result.rb
@@ -7,7 +7,7 @@ class RunResult < ApplicationRecord
   def visualization_url
     return unless result_file_url
 
-    "http://visualization.osemosys-cloud.com/?model=#{CGI.escape(result_file_url)}"
+    "http://visualization.osemosys-cloud.com/?model=#{CGI.escape(result_file_url)}&locale=#{run.language}" # # rubocop:disable Layout/LineLength
   end
 
   def result_file_url
