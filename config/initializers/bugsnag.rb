12da2288ffdf20f032c8f4b34518b1dc1c5481f3
Youssef Boulkaid <youssef@yboulkaid.com>
2022-04-05 17:06:49 +0200

    Don’t report external errors to bugsnag

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,5 +1,6 @@
 if Rails.env.production?
   Bugsnag.configure do |config|
     config.api_key = Rails.application.credentials.dig(:bugsnag, :api_key)
+    config.discard_classes << 'TTY::Command::ExitError'
   end
 end
