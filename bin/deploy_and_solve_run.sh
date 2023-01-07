7532234e85f74c38403b4908d693549181717551
Youssef Boulkaid <youssef@yboulkaid.com>
2023-01-07 11:17:34 +0100

    More robust script

--- bin/deploy_and_solve_run.sh
+++ bin/deploy_and_solve_run.sh
@@ -1,13 +1,19 @@
 #bin/sh
 
-# Don't set -e, or the machines won't stop on failure!
-# set -e
-
 # https://www.davidpashley.com/articles/writing-robust-shell-scripts/
-set -xu
+set -exu
 
 run_id=$1
 shutdown_on_finish=$2
+
+terminate_instance() {
+  echo "Terminating instance now..."
+  if $shutdown_on_finish; then
+    sudo shutdown -h now
+  fi
+}
+
+trap "terminate_instance" ERR
 
 cd /home/ubuntu/osemosys-cloud/ || exit
 /home/ubuntu/.asdf/shims/bundle install
@@ -15,7 +21,4 @@ pip3 install -r requirements.txt
 
 /home/ubuntu/.asdf/shims/bundle exec rake solve_cbc_run[$run_id]
 
-if $shutdown_on_finish; then
-  # echo "Shutting down"
-  sudo shutdown -h now
-fi
+terminate_instance
